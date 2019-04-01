3ae5f3664d3b2ba9456ec4beb8102df1e0f293c5
Sergey Panin <spanin@topcon.com>
2019-04-01 23:30:32 +0300

    Added snappy project

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # theI18n
 
 The translate package
+
+### How to generate rocksdb
+* cd projects
+* python GenRocksDb.py
+
--- projects/GenRocksDb.py
+++ projects/GenRocksDb.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 # -*-coding: UTF-8 -*-
+
 import os
 import shutil
 
-out="build/cmake-projects/the/"
-h="projects/perfect"
+out="rocksdb/build/"
+project_dir="rocksdb"
 cmake="cmake"
-s="Visual Studio 15 2017 Win64"
+compiler="Visual Studio 15 2017 Win64"
+msbuild="c:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional\\MSBuild\\15.0\\Bin\\msbuild.exe"
+mode="debug"
 
 if (__name__ == "__main__"):
     print("--- Begin ---")
-    if(os.path.exists(out) is True):
-        shutil.rmtree(out)
-    os.makedirs(out)
-    os.system("{} -G\"{}\" -B{} -H{} -DWITH_GFLAGS=1 -DWITH_SNAPPY=1 -DWITH_JEMALLOC=1 -DWITH_JNI=1".format(cmake, s, out, h))
+    #if(os.path.exists(out) is True):
+        #shutil.rmtree(out)
+    #os.makedirs(out)
+    #os.system(f"{cmake} -G\"{compiler}\" -B{out} -H{project_dir} -DWITH_GFLAGS=1 -DWITH_SNAPPY=1 -DWITH_JEMALLOC=1 -DWITH_JNI=1")
+    if(mode == "debug"):
+        os.system(f"cd {project_dir} && \"{msbuild}\" rocksdb.sln /m")
+    else:
+        os.system(f"cd {project_dir} && \"{msbuild}\" rocksdb.sln /p:Configuration=Release")
     print("--- End ---")
 
-# sample command: cmake -G "Visual Studio 15 Win64" -DWITH_GFLAGS=1 -DWITH_SNAPPY=1 -DWITH_JEMALLOC=1 -DWITH_JNI=1 ..
