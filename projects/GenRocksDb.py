9ce1e50c590a5306cb8046df712eaf7efdab1af5
Sergey Panin <spanin@topcon.com>
2019-04-01 23:37:06 +0300

    Update projects

--- projects/GenRocksDb.py
+++ projects/GenRocksDb.py
@@ -5,6 +5,7 @@ import os
 import shutil
 
 out="rocksdb/build/"
+snappy_out="snappy/build/"
 project_dir="rocksdb"
 cmake="cmake"
 compiler="Visual Studio 15 2017 Win64"
@@ -13,6 +14,12 @@ mode="debug"
 
 if (__name__ == "__main__"):
     print("--- Begin ---")
+    print("--- Build Snappy ---")
+    if(os.path.exists(snappy_out) is True):
+        shutil.rmtree(snappy_out)
+    os.makedirs(snappy_out)
+    os.system(f"{cmake} -G\"{compiler}\" -B{out} -H{project_dir} -DWITH_GFLAGS=1 -DWITH_SNAPPY=1 -DWITH_JEMALLOC=1 -DWITH_JNI=1")
+    print("--- End Snappy ---")
     #if(os.path.exists(out) is True):
         #shutil.rmtree(out)
     #os.makedirs(out)
