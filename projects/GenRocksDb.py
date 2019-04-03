5443939e4cffa10835b87b6e714600e7bdc3e88d
Sergey Panin <spanin@topcon.com>
2019-04-03 22:47:53 +0300

    Remove rocksdb project

--- projects/GenRocksDb.py
+++ projects/GenRocksDb.py
@@ -4,32 +4,38 @@
 import os
 import shutil
 
-out="rocksdb/build/"
-project_dir="rocksdb"
 cmake="cmake"
 compiler="Visual Studio 15 2017 Win64"
 msbuild="c:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional\\MSBuild\\15.0\\Bin\\msbuild.exe"
-mode="debug"
+mode="debug"  # debug or release
 
 
-def createTree(path):
-    if(os.path.exists(path) is True):
-        shutil.rmtree(path)
-    os.makedirs(path)
+
+class Lib():
+    def __init__(self, name):
+        self.name_ = name
+        self.path_ = name + "/build"
+    def createTree(self):
+        if(os.path.exists(self.path_) is True):
+            shutil.rmtree(self.path_)
+        os.makedirs(self.path_)
+    def exec(self):
+        self.createTree()
+        os.system(f"{cmake} -G\"{compiler}\" -B{self.path_} -H{self.name_}")
+        if(mode == "debug"):
+            os.system(f"cd {self.path_} && \"{msbuild}\" {self.name_}.sln /m")
+        else:
+            os.system(f"cd {self.path_} && \"{msbuild}\" {self.name_}.sln /p:Configuration=Release")
+
+def createLib(name):
+    lib = Lib(name)
+    lib.exec()
 
 
 if (__name__ == "__main__"):
-    print("--- Begin ---")
-    print("--- Build Snappy ---")
-    createTree("snappy/build/")
-    #mkdir build cd build && cmake ../ && make
-    os.system(f"{cmake} -G\"{compiler}\" -B\"snappy/build\" -H\"snappy\"")
-    print("--- End Snappy ---")
-    #createTree("rocksdb/build/")
-    #os.system(f"{cmake} -G\"{compiler}\" -B\"rocksdb/build\" -H{project_dir} -DWITH_GFLAGS=1 -DWITH_SNAPPY=1 -DWITH_JEMALLOC=1 -DWITH_JNI=1")
-    #if(mode == "debug"):
-        #os.system(f"cd {project_dir} && \"{msbuild}\" rocksdb.sln /m")
-    #else:
-        #os.system(f"cd {project_dir} && \"{msbuild}\" rocksdb.sln /p:Configuration=Release")
-    print("--- End ---")
+    print("=== Begin ===")
+    print("=== Build Snappy ===")
+    createLib("snappy")
+    print("=== End Snappy ===")
+    print("=== End ===")
 
